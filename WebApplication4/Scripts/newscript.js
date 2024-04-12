e00f05e237e8d24793d2aa86f202ff9141f86173
izkesu <ksingh8354990057@gmail.com>
2024-04-12 18:32:42 +0530

    fina

---
+++ WebApplication4/Scripts/newscript.js
@@ -0,0 +1,18 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    $('#imageModal').on('show.bs.modal', function (e) {
+        // Change border color
+        $('.modal-content').css('border-color', '#dc3545'); // Red border
+
+        // Change background color of the modal header
+        $('.modal-header').css('background-color', '#dc3545'); // Red background
+    });
+
+    $('#imageModal').on('hide.bs.modal', function (e) {
+        // Reset border color
+        $('.modal-content').css('border-color', '#007bff'); // Blue border
+
+        // Reset background color of the modal header
+        $('.modal-header').css('background-color', '#007bff'); // Blue background
+    });
+});
+s
